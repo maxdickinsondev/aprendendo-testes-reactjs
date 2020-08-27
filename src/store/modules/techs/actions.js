e29230985a8aed1dea4ecec413cb3b27d73cfcbe
Max Dickinson <maxsantistadickinson@gmail.com>
2020-08-27 09:19:41 -0300

    Testando sagas com mock de api.

--- src/store/modules/techs/actions.js
+++ src/store/modules/techs/actions.js
@@ -6,3 +6,18 @@ export function addTech(tech) {
         }
     }
 }
+
+export function getTechsSuccess(data) {
+    return {
+        type: 'GET_TECH_SUCCESS',
+        payload: {
+            data
+        }
+    }
+}
+
+export function getTechsFailure() {
+    return {
+        type: 'GET_TECHS_FAILURE',
+    }
+}
